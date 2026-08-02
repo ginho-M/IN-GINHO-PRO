5ae706ae3b9ad57aa363a96cdbbc05e2615a2993
ginho-M <ginhomarie@gmail.com>
2026-08-02 23:49:26 +0400

    Fix: ajout du port dynamique pour Render

--- app.py
+++ app.py
@@ -2089,4 +2089,6 @@ if __name__ == '__main__':
     if has_update:
         print(f"🔄 Une nouvelle version ({version}) est disponible.")
     
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    import os
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=False, host='0.0.0.0', port=port)
